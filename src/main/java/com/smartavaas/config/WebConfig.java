5c7d7877e2ccc2dc8cdfd7e19caa60ef7ba5e072
kahar12911 <156943281+kahar12911@users.noreply.github.com>
2025-05-31 01:07:13 +0530

    Update WebConfig.java
    Updated end point to from local to https://develop--adorable-liger-d685f6.netlify.app

--- src/main/java/com/smartavaas/config/WebConfig.java
+++ src/main/java/com/smartavaas/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("https://develop--adorable-liger-d685f6.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
